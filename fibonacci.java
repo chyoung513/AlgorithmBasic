cdb2e04865dd20535911c79104a05428575c3977
chyoung513 <jxy5130@gmail.com>
2018-08-29 20:31:17 +0900

    +add
    1. get sum of 1 to n
    2. fibonacci number test

---
+++ fibonacci.java
@@ -0,0 +1,12 @@
+public class Fibonacci {
+    public static void main(String args[]) {
+        int f[]=new int[10];
+        f[0] = 0;
+        f[1] = 1;
+        
+        for(int i = 2 ; i < 10 ; i++){
+            f[i] = f[i-1] + f[i-2];
+        }
+
+    }
+}
